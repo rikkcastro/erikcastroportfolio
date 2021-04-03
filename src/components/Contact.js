b97338f4f9863b8d549bd3def556645f3f098ecb
Erik Castro <castro.erikk@gmail.com>
2021-04-03 21:58:16 +0800

    6th

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/core/styles"
-import{ TextField, Typography, Button, Grid, Box} from '@material-ui/core'
-import SendIcon from "@material-ui/icons/Send"
+
 
 const Contact = () => {
     return (
